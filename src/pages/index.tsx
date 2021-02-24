ede41cd17de13280bc5c1e88e86325f22159ed56
Dimas Resende <dresende@uolinc.com>
2021-02-24 20:38:08 -0300

    Criando layout da tela de desafios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+
 import styles from "../styles/pages/Home.module.css";
 import Head from 'next/head';
 
@@ -19,7 +21,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
